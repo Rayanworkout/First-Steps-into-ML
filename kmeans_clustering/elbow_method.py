e604b351bd03055b10df82c68c80f3b5948c4978
Rayan <rayan13170@protonmail.com>
2024-04-01 16:40:01 +0200

    added separate files for functions

---
+++ kmeans_clustering/elbow_method.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+def show_elbow_chart( X_train: np.ndarray, max_clusters: int) -> None:
+    """
+    Display the chart of the elbow method.
+
+    Parameters
+    ----------
+    max_clusters : int
+        The maximum number of clusters to iterate through.
+    
+    X_train : np.ndarray
+        The training data.
+
+    """
+    fits = []
+    scores = []
+    
+    K = range(2, max_clusters + 1)
+    
+    for k in K:
+        # Train the model for the current value of k on training data
+        model = KMeans(n_clusters=k, random_state=0, n_init='auto').fit(X_train)
+        
+        # Append the model to fits
+        fits.append(model)
+
+        # Calculate the silhouette score
+        score = silhouette_score(X_train, model.labels_, metric='euclidean')
+        
+        # Append the silhouette score to scores
+        scores.append(score)
+    
+    # Plotting the elbow chart
+    plt.plot(K, scores, marker='o')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Silhouette Score')
+    plt.title('Elbow Method For Optimal k')
+    plt.show()
--- kmeans_clustering/silhouette_score.py
+++ kmeans_clustering/silhouette_score.py
@@ -1,9 +1,12 @@
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 import numpy as np
 
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
-def get_silhouette_score(X: np.ndarray, k: int):
+import matplotlib.pyplot as plt
+
+
+def get_silhouette_score(X: np.ndarray, k: int) -> int:
     """
     This function returns the best number of clusters based on the silhouette score.
     The silhouette analysis can be used to study the separation distance between the resulting clusters.
@@ -20,11 +23,20 @@ def get_silhouette_score(X: np.ndarray, k: int):
     scores = []
 
     # For each number of clusters, calculate the silhouette score
-    for n_clusters in range(2, k):
+    for n_clusters in range(2, k + 1):
         clusterer = KMeans(init="k-means++", n_clusters=n_clusters, random_state=42)
+        
         y = clusterer.fit_predict(X)
 
-        scores.append((n_clusters, silhouette_score(X, y)))
+        score = silhouette_score(X, y)
+
+        scores.append((n_clusters, score))
+    
+    plt.plot(range(2, k + 1), scores, marker='o')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Silhouette Score')
+    plt.title('Elbow Method For Optimal k')
+    plt.show()
 
     # And we return the number of clusters with the highest silhouette score
     return max(scores, key=lambda x: x[1])[0]
