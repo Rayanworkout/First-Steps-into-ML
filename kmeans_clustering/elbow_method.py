34eb7ec230542e8b59203d6accf6f78f5ace59de
Rayan <rayan13170@protonmail.com>
2024-04-01 22:55:15 +0200

    enhanced companies analytics

--- kmeans_clustering/elbow_method.py
+++ kmeans_clustering/elbow_method.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-def show_elbow_chart( X_train: np.ndarray, max_clusters: int) -> None:
+
+def show_elbow_chart(X_train: np.ndarray, max_clusters: int) -> None:
     """
     Display the chart of the elbow method.
 
@@ -11,32 +12,32 @@ def show_elbow_chart( X_train: np.ndarray, max_clusters: int) -> None:
     ----------
     max_clusters : int
         The maximum number of clusters to iterate through.
-    
+
     X_train : np.ndarray
         The training data.
 
     """
     fits = []
     scores = []
-    
+
     K = range(2, max_clusters + 1)
-    
+
     for k in K:
         # Train the model for the current value of k on training data
-        model = KMeans(n_clusters=k, random_state=0, n_init='auto').fit(X_train)
-        
+        model = KMeans(n_clusters=k, random_state=0, n_init="auto").fit(X_train)
+
         # Append the model to fits
         fits.append(model)
 
         # Calculate the silhouette score
-        score = silhouette_score(X_train, model.labels_, metric='euclidean')
-        
+        score = silhouette_score(X_train, model.labels_, metric="euclidean")
+
         # Append the silhouette score to scores
         scores.append(score)
-    
+
     # Plotting the elbow chart
-    plt.plot(K, scores, marker='o')
-    plt.xlabel('Number of Clusters')
-    plt.ylabel('Silhouette Score')
-    plt.title('Elbow Method For Optimal k')
+    plt.plot(K, scores, marker="o")
+    plt.xlabel("Number of Clusters")
+    plt.ylabel("Silhouette Score")
+    plt.title("Elbow Method For Optimal k")
     plt.show()
